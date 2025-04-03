5f9e51a1760b0ed36188c7ce6790e2143dd74249
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-03 16:26:28 +0330

    feat: add pricing service to the project

---
+++ src/app/pricing.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PricingService {
+  pricings: PriceCard[] = [
+    {
+      plan: 'Basic',
+      storage: '100GB',
+      price: {
+        amount: 1.99,
+        currency: 'USD',
+        billing_cycle: 'Month',
+      },
+      features: [
+        '100 GB of storage',
+        'Option to add members',
+        'Extra member benefits',
+      ],
+      purchase_link: '#',
+      featured: false,
+    },
+    {
+      plan: 'Standard',
+      storage: '200GB',
+      price: {
+        amount: 3.99,
+        currency: 'USD',
+        billing_cycle: 'Month',
+      },
+      features: [
+        '200 GB of storage',
+        'Option to add members',
+        'Extra member benefits',
+      ],
+      purchase_link: '#',
+      featured: false,
+    },
+    {
+      plan: 'Premium',
+      storage: '2 TB',
+      price: {
+        amount: 8.99,
+        currency: 'USD',
+        billing_cycle: 'Month',
+      },
+      features: [
+        '2 TB of storage',
+        'Option to add members',
+        'Extra member benefits',
+      ],
+      purchase_link: '#',
+      featured: false,
+    },
+  ];
+
+  constructor() {}
+
+  get plans() {
+    return this.pricings;
+  }
+}
