53eaeba38ee280a8ee5fcee5867754c0b56479ae
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-03 16:27:53 +0330

    feat: inject and get pricing plans form pricing service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PricingCardComponent } from './pricing-card/pricing-card.component';
+import { PricingService } from './pricing.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PricingCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'pricing-grids';
+  pricingService = inject(PricingService);
+  plans = this.pricingService.pricings;
 }
