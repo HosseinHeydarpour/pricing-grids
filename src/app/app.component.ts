13f57e8920fc03783c30fb88daa47ee83f7c127d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-03 16:44:28 +0330

    feat: add price card plans to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PricingCardComponent } from './pricing-card/pricing-card.component';
 import { PricingService } from './pricing.service';
+import { CommonModule } from '@angular/common';
+import { PriceCard } from './price-card.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PricingCardComponent],
+  imports: [PricingCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   pricingService = inject(PricingService);
-  plans = this.pricingService.pricings;
+  plans: PriceCard[] = this.pricingService.pricings;
 }
